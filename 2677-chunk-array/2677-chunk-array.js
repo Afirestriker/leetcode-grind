a050ba09ddcbcd1c6f501fadef2e167598b16aec
Ashish Agrawal <89931316+Afirestriker@users.noreply.github.com>
2024-03-12 09:00:11 +0530

    Time: 63 ms (19.55%), Space: 51.2 MB (60.08%) - LeetHub

--- 2677-chunk-array/2677-chunk-array.js
+++ 2677-chunk-array/2677-chunk-array.js
@@ -4,22 +4,14 @@
  * @return {Array}
  */
 var chunk = function(arr, size) {
-    if (!arr.length) return [];
+    if (!arr.length || size === 0) return [];
 
-    const maxChunkableIndex = Math.floor(arr.length / size) * size;
     const chunkedArray = [];
     let indx = 0;
 
-    while (indx <= maxChunkableIndex) {
-        const chunk = arr.slice(indx);
-
-        if (indx !== maxChunkableIndex) chunk.length = size;
-
-        chunkedArray.push(chunk);
-
+    while (indx < arr.length) {
+        chunkedArray.push(arr.slice(indx, indx + size));
         indx += size;
-
-        if (indx >= arr.length) break;
     }
 
     return chunkedArray;
