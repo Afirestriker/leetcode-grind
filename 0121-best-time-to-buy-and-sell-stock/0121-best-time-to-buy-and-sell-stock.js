5c13c2c465d29cd2f093cd76d42ffd363e2e1b70
Ashish Agrawal <89931316+Afirestriker@users.noreply.github.com>
2024-08-04 14:31:34 +0530

    Time: 74 ms (59.72%), Space: 59.1 MB (50.21%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,20 +3,13 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let buy = Infinity;
-    let sell = 0;
+    let min = prices[0];
     let profit = 0;
 
     prices.forEach(price => {
-        if (price < buy) {
-            buy = price;
-            sell = 0;
-        }
-
-        sell = price > buy ? price : sell;
-
-        if (sell > 0) profit = (sell - buy > profit) ? sell - buy : profit;
-
+        const cost = price - min;
+        profit = Math.max(cost, profit);
+        min = Math.min(price, min);
     });
 
     return profit;
